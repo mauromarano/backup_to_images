8624f5ef08222bbd9886d180aa643522737f3191
Mauro <gagginaspinnata@gmail.com>
2016-04-18 22:21:54 +0200

    added a lot of things

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strict";
 
-var _webBundle = require("web-bundle");
-
-var _webBundle2 = _interopRequireDefault(_webBundle);
-
 var _async = require("async");
 
 var _async2 = _interopRequireDefault(_async);
@@ -12,12 +8,38 @@ var _shelljs = require("shelljs");
 
 var _shelljs2 = _interopRequireDefault(_shelljs);
 
+var _readline = require("readline");
+
+var _readline2 = _interopRequireDefault(_readline);
+
+var _randomstring = require("randomstring");
+
+var _randomstring2 = _interopRequireDefault(_randomstring);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Backup = function Backup() {
+    var _this = this;
+
     _classCallCheck(this, Backup);
+
+    // VARIABLES
+    var self = this;
+
+    // FUNCTIONS
+    this.ask = function (question, cb) {
+        var self = _this;
+        var rl = _readline2.default.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(question, function (answer) {
+            rl.close();
+            cb(null, answer);
+        });
+    };
 
     this.encode = function (input, output, cb) {
         _shelljs2.default.exec("node node_modules/web-bundle/tool/wb.js encode " + input + " -o " + output + " "), function () {
@@ -30,10 +52,40 @@ var Backup = function Backup() {
             cb();
         };
     };
+
+    _async2.default.series([function (cb) {
+        _this.ask("Type in the size (MB) of each block: ", function (err, res) {
+            cb(null, res);
+        });
+    }, function (cb) {
+        _this.ask("Type in your password: ", function (err, res) {
+            cb(null, res);
+        });
+    }, function (cb) {
+        _this.ask("How would you like to name the archive: ", function (err, res) {
+            cb(null, res);
+        });
+    }, function (cb) {
+        _this.ask("Type in the name of the folder you want to split and compress: ", function (err, res) {
+            cb(null, res);
+        });
+    }], function (err, res) {
+        var size = res[0] * 1024;
+        var password = res[1];
+        var archive_name = res[2];
+        var folder_name = res[3];
+        console.log("<==============================>\nThe size of each block will be " + size + "KB\nYou password is " + password + "\nThe name of the archive is " + archive_name + "\nThe file you want to compress and split is " + folder_name + "\n            ");
+
+        _this.ask('Please press enter to continue', function (err, res) {
+            _shelljs2.default.exec("rar a -v" + size + "k -df -hp" + password + " -m0 " + archive_name + ".rar " + folder_name, function () {
+                console.log('done');
+            });
+        });
+    });
 };
 
 var backup = new Backup();
-backup.encode('doc.ods.zip', 'immagine.png');
-backup.decode('immagine.png', 'doc2.ods.zip', function () {
-    console.log('done');
-});
+// backup.encode('doc.ods.zip','immagine.png');
+// backup.decode('immagine.png', 'doc2.ods.zip', function() {
+//     console.log('done');
+// });
--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
-import wb from "web-bundle"
 import async from "async"
 import shell from "shelljs"
+import readline from "readline"
+import randomstring from "randomstring"
+
 class Backup {
     constructor() {
+
+        // VARIABLES
+        const self = this;
+
+
+
+        // FUNCTIONS
+        this.ask = (question, cb) => {
+            const self = this;
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question(question, (answer) => {
+                rl.close();
+                cb(null, answer);
+            });
+        }
+
 
         this.encode = (input, output, cb) => {
             shell.exec(`node node_modules/web-bundle/tool/wb.js encode ${input} -o ${output} `),
@@ -17,11 +38,45 @@ class Backup {
                 cb();
             };
         }
+
+        async.series([
+            (cb) => {
+                this.ask("Type in the size (MB) of each block: ", (err, res) => {
+                    cb(null, res);
+                });
+            }, (cb) => {
+                this.ask("Type in your password: ", (err, res) => {
+                    cb(null, res);
+                });
+            }, (cb) => {
+                this.ask("How would you like to name the archive: ", (err, res) => {
+                    cb(null, res);
+                });
+            }, (cb) => {
+                this.ask("Type in the name of the folder you want to split and compress: ", (err, res) => {
+                    cb(null, res);
+                });
+            }
+
+        ], (err, res)=> {
+            let size = res[0]*1024;
+            let password = res[1];
+            let archive_name = res[2];
+            let folder_name = res[3];
+            console.log(`<==============================>\nThe size of each block will be ${size}KB\nYou password is ${password}\nThe name of the archive is ${archive_name}\nThe file you want to compress and split is ${folder_name}
+            `);
+
+            this.ask('Please press enter to continue',(err,res)=>{
+                shell.exec(`rar a -v${size}k -df -hp${password} -m0 ${archive_name}.rar ${folder_name}`,()=>{
+                    console.log('done');
+                });
+            });
+        });
     }
 }
 
 let backup = new Backup();
-backup.encode('doc.ods.zip','immagine.png');
-backup.decode('immagine.png', 'doc2.ods.zip', function() {
-    console.log('done');
-});
+// backup.encode('doc.ods.zip','immagine.png');
+// backup.decode('immagine.png', 'doc2.ods.zip', function() {
+//     console.log('done');
+// });
